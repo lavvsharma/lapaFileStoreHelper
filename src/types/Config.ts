3fcd26aaa643d89ad29da9e2e56f64a08ffd94a4
lavvsharma <lavsharma2016@gmail.com>
2024-01-30 11:37:23 +0530

    Config.ts added for lapaFileStoreUrl

---
+++ src/types/Config.ts
@@ -0,0 +1,5 @@
+interface Config {
+  lapaFileStoreUrl: String;
+  }
+  
+  export default Config;
---
+++ src/utils/config.ts
@@ -0,0 +1,7 @@
+import type Config from "../types/Config";
+
+const config: Config = {
+  lapaFileStoreUrl: "localhost",
+};
+
+export default config;
